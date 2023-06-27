9d822c19a6d1132dcad0f97991019278d7ac0db1
dplus <altersun@gmail.com>
2023-06-26 23:05:31 -0400

    it works, mostly

--- include/before_main.hpp
+++ include/before_main.hpp
@@ -1,24 +1,27 @@
-
-#include <iostream>
+#include <functional>
 #include <string>
 #include <string_view>
 
-using namespace std;
-static const string str(__FUNCTION__);
+// Shout out to stackoverflow contributers:
+// https://stackoverflow.com/a/1597129
+#define UNIQUE_NAME_INNER(x, y) x ## y
+#define UNIQUE_NAME_OUTER(x, y) UNIQUE_NAME_INNER(x, y)
+#define UNIQUE(Name) UNIQUE_NAME_OUTER(Name, __LINE__)
 
 
 constexpr bool check_top_level(char const * scope) {
     return std::string_view("top level")==scope;
 }
 
-constexpr bool check_main(char const * scope) {
-    return std::string_view("main")==scope;
-}
     
-#define CHECK_TOP_LEVEL   static_assert(check_top_level(__FUNCTION__), "Top level!")
-#define CHECK_MAIN        static_assert(check_main(__FUNCTION__), "Main!")
-
-//CHECK_MAIN;
-CHECK_TOP_LEVEL;
+#define CHECK_TOP_LEVEL   static_assert(check_top_level(__FUNCTION__), "Not defined at top level!")
 
 
+struct BeforeMain {
+    BeforeMain(std::function<void()> funk) {funk();}
+};
+
+
+
+#define BEFORE_MAIN(id, funk)   CHECK_TOP_LEVEL;static BeforeMain UNIQUE(id)(funk);
+ 
--- test/before_main_test.cpp
+++ test/before_main_test.cpp
@@ -1,51 +1,19 @@
-
 #include <iostream>
-#include <functional>
-#include <string>
-#include <string_view>
 
-using namespace std;
-static const string str(__FUNCTION__);
+#include "../include/before_main.hpp"
 
 
-constexpr bool check_top_level(char const * scope) {
-    return std::string_view("top level")==scope;
-}
-
-constexpr bool check_main(char const * scope) {
-    return std::string_view("main")==scope;
-}
-    
-#define CHECK_TOP_LEVEL   static_assert(check_top_level(__FUNCTION__), "Top level!")
-#define CHECK_MAIN        static_assert(check_main(__FUNCTION__), "Main!")
-
-
-struct BeforeMain {
-    BeforeMain(std::function<void()> funk) {funk();}
-};
 
 void say_whatup()
 {
     std::cout << "WHATUPPPP" << std::endl;
 }
 
-#define BEFORE_MAIN(funk)   CHECK_TOP_LEVEL;static\
- BeforeMain f(funk);
 
-
-
-BEFORE_MAIN(say_whatup);
-
-
-//CHECK_MAIN;
-//CHECK_TOP_LEVEL;
-
+BEFORE_MAIN(first,say_whatup);
 
 int main()
 {
-    cout<<__FUNCTION__<<endl;
-    cout<<str<<endl;
-    //CHECK_TOP_LEVEL;
-    CHECK_MAIN;
+    std::cout << "Actual main" << std::endl;
     return 0;
 }
