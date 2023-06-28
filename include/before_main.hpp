f0fa6d7d91a3a5b863b847063e2323afa87efdff
dplus <altersun@gmail.com>
2023-06-28 17:39:11 -0400

    solved arg problem

--- include/before_main.hpp
+++ include/before_main.hpp
@@ -4,10 +4,19 @@
 #include "top_level.hpp"
 #include "unique_name.hpp"
 
-
+/*
 struct BeforeMain {
     BeforeMain(std::function<void()> funk) {funk();}
 };
+*/
 
-#define BEFORE_MAIN(funk)   CHECK_TOP_LEVEL; static BeforeMain UNIQUE_NAME (funk)
+struct BeforeMain {
+    BeforeMain(std::function<void()> func) {func();}
+    template <class Function, class ... Args>
+    BeforeMain(Function func, Args...args) {func(args...);}
+
+};
+
+#define BEFORE_MAIN(func)                  CHECK_TOP_LEVEL; static BeforeMain UNIQUE_NAME (func)
+#define BEFORE_MAIN_WITH_ARGS(func, ...)   CHECK_TOP_LEVEL; static BeforeMain UNIQUE_NAME (func, __VA_ARGS__)
  
--- test/before_main_test.cpp
+++ test/before_main_test.cpp
@@ -3,18 +3,22 @@
 #include "../include/before_main.hpp"
 
 
-void say_whatup()
+void say_whatup(void)
 {
     static int count = 0;
     std::cout << "WHATUPPPP " << count++ << std::endl;
 }
 
+void say_two_integers(int a, int b)
+{
+    std::cout << "Two ints for ya: " << a << " and " << b << std::endl;
+}
+
 
 BEFORE_MAIN(say_whatup);
+BEFORE_MAIN_WITH_ARGS(say_two_integers, 45, 67);
 BEFORE_MAIN(say_whatup);
-BEFORE_MAIN(say_whatup);
-BEFORE_MAIN(say_whatup);
-BEFORE_MAIN(say_whatup);
+BEFORE_MAIN_WITH_ARGS(say_two_integers, 21, 600);
 
 
 int main()
